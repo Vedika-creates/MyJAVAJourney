913e7ab23a35dbd2f053a61436c3b7f66b851da7
Vedika <vedikak1312@gmail.com>
2025-07-20 13:43:14 +0530

    Add files via upload

---
+++ Ternary_opt.java
@@ -0,0 +1,12 @@
+public class Ternary_opt {
+    public static void main(String a[]){
+        int x=4;
+        // int y=10;
+        // int z=20;
+        int result =0;
+        // variable = condition ? if condition is true : if condition is false
+        result= x%2==0 ? 10 : 20;
+        System.out.println(result);
+    }
+    
+}
