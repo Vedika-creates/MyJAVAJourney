1f45c9d6b39abf5f994232669e4bbbd35d7fe77f
Vedika <vedikak1312@gmail.com>
2025-07-20 13:42:43 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    public static void main(String a[]){
+        int x=10;
+        int y=50;
+        int z=100;
+        if(x>y && x>z)
+          System.out.println(x);
+        else if ( y>z)
+          System.out.println(y);
+        else
+          System.out.println(z);
+
+    }
+}
